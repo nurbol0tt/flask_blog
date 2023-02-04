59aa4144551043a6eba4aaf8659ba035ed17ae8b
nura <fairtail764@gmail.com>
2023-02-04 14:08:36 +0600

    add profile

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    joined = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='user', lazy=True)
@@ -40,8 +42,8 @@ class User(db.Model, UserMixin):
             return None
         return User.query.get(user_id)
 
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+    # def __repr__(self):
+    #     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Post(db.Model):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request
+from flask_login import current_user
 
-from blog.models import Post
+from blog.models import Post, User
 
 main = Blueprint('main', __name__, template_folder='templates')
 
@@ -10,5 +11,12 @@ main = Blueprint('main', __name__, template_folder='templates')
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('index.html', posts=posts)
-
+    if current_user.is_anonymous:
+        print("*"*20)
+        return render_template('index.html', posts=posts)
+    else:
+        user = User.query.filter_by(id=current_user.id).first()
+        print(user.__dir__())
+        return render_template('index.html', posts=posts, user=user)
+    #
+    # return render_template('index.html', posts=posts)
